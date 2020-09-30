4ad9fde88c00d35eb0ebd9999907449c4dc48624
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-09-30 21:57:51 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField, Dialog, DialogTitle, DialogContent } from '@material-ui/core';
 
+const iframeStyle = {
+  width: "100%",
+  height: "640px",
+};
+
+const walletOrigin = "https://yopta.net";
+// const walletOrigin = "https://walet.velas.com"; prod - not working yet
 function App() {
+  const [to, setTo] = React.useState("");
+  const [data, setData] = React.useState("");
+  const [amount, setAmount] = React.useState("0");
+  const [opened, setOpened] = React.useState(false);
+  const openWallet = () => setOpened(true);
+  const closeWallet = () => setOpened(false);
+  const iframeSrc = `${walletOrigin}#to=${to}&transaction=${data}&amount=${amount}`;
+  const onMessage = (event) => {
+    if (event.origin !== walletOrigin) {
+      return;
+    }
+    try {
+      const json = JSON.parse(event.data);
+      if (json.err) {
+        throw new Error(json.err);
+      }
+      alert(json.txHash);
+    } catch(e) {
+      alert(e.message);
+    }
+    closeWallet();
+  };
+  React.useEffect(() => {
+    window.addEventListener("message", onMessage);
+    return () => window.removeEventListener("message", onMessage);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form noValidate autoComplete="off">
+      <TextField id="standard-basic" label="To(vlx or eth format)" onChange={(event) => setTo(event.target.value)} value={to} />
+      <TextField id="filled-basic" label="Data(hex)" onChange={(event) => setData(event.target.value)} value={data} />
+      <TextField id="outlined-basic" label="Amount" onChange={(event) => setAmount(event.target.value)} value={amount}/>
+      <Button variant="contained" onClick={openWallet}>Open</Button>
+      <Dialog open={opened} onClose={closeWallet}>
+          <DialogTitle id="customized-dialog-title" onClose={closeWallet}>
+            Sign your transaction
+          </DialogTitle>
+          <DialogContent dividers>
+              <iframe style={iframeStyle} src={iframeSrc}></iframe>
+          </DialogContent>
+      </Dialog>
+    </form>
   );
 }
-
 export default App;
